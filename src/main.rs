266f1c814f5add0eef3b0e1c71d49075afeac877
Koutako2024 <koutako2021@yahoo.co.jp>
2025-12-26 00:43:41 +0900

    make to load data from file.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use rand::random_range;
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
 use std::thread::sleep;
 use std::time::Duration;
 use unicode_segmentation::UnicodeSegmentation;
@@ -28,11 +30,6 @@ impl<'a> MarkovGenerator<'a> {
                     let v = e2e.entry(splited[i]).or_insert(Vec::new());
                     v.push(splited[i + 1]);
                 }
-                //
-                // // last element may not be pushed
-                // if let Some(last_element) = splited.last() {
-                //     e2e.entry(*last_element).or_insert(vec!["<end>"]);
-                // }
             }
         }
     }
@@ -63,48 +60,24 @@ impl<'a> MarkovGenerator<'a> {
             .for_each(|element| generated_str.push_str(element));
 
         generated_str
-
-        // if let Some(e2e) = &self.e2e {
-        //     if let Some(element) = e2e.keys().nth(random_range(0..e2e.keys().len())) {
-        //         let mut generated = vec![*element];
-        //         loop {
-        //             let hoge = e2e[generated.last().expect("wtf")]
-        //                 [random_range(0..e2e[generated.last().expect("wtf")].len())];
-
-        //             if hoge == "\n" {
-        //                 break;
-        //             }
-
-        //             generated.push(hoge);
-        //         }
-
-        //         let mut generated_str = String::new();
-        //         generated
-        //             .iter()
-        //             .for_each(|element| generated_str.push_str(element));
-
-        //         generated_str
-        //     } else {
-        //         panic!("wtf");
-        //     }
-        // } else {
-        //     panic!("e2e has not setup yet!");
-        // }
     }
 }
 
 fn main() {
+    let filepath = "./data.txt";
+    println!("Open {},,,.", filepath);
+    let mut f = File::open(filepath).expect("File not found!");
+    println!("File has opened successfully!");
+
+    println!("Load content,,,.");
+    let mut content = String::new();
+    f.read_to_string(&mut content)
+        .expect("Something went wrong reading file!");
+    println!("Content has loaded successfully!");
+
     println!("Setup markov generator,,,.");
     let mut generator = MarkovGenerator {
-        raw_text: Some(
-            "
-重役募集ラストスパート
-寒風の吹きすさぶ折、皆様いかがお過ごしでしょうか。
-さて、次期重役の椅子も現在あと少しとなっております。とはいえ、あと少しといっても全て埋める必要があります。
-皆様のご連絡、切にお待ちしております。
-ピン留めにある、既に申し込まれた御学友と共に放課後に議論する。中々ステキな時間だと感じます。
-",
-        ),
+        raw_text: Some(&content),
         splited: None,
         e2e: None,
     };
@@ -113,6 +86,7 @@ fn main() {
     println!("raw_text: {:?}", generator.raw_text);
     println!("splited: {:?}", generator.splited);
     println!("e2e: {:?}", generator.e2e);
+
     println!("Start generating.");
     let duration = Duration::from_millis(500);
     loop {
